a20364f60f2c434d546d9d4d190cdd2752d8aa95
Ghassen CHARNI <50666854+ghassencherni@users.noreply.github.com>
2024-02-11 20:04:38 +0100

    Delete eks directory

--- eks-graphenedb/outputs.tf
+++
@@ -1,9 +0,0 @@
-#----eks-graphenedb/outputs.tf----
-
-output "cluster-endpoint" {
-  value = "${aws_eks_cluster.graphenedb-cluster.endpoint}"
-}
-
-output "cert-auth" {
-  value = "${aws_eks_cluster.graphenedb-cluster.certificate_authority.0.data}"
-}
--- eks-graphenedb/variables.tf
+++
@@ -1,9 +0,0 @@
-#----eks-graphenedb/variables.tf----
-
-variable "aws_region" {}
-
-variable "graphenedb_public_subnets_ids" {
-  type = "list"
-}
-
-variable "graphenedb_public_sg_id" {}
